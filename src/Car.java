9f405a484e9bef0cf865184b8ebf873178d70dbc
Heyvkram <mrc.luq.com>
2025-01-28 12:11:40 +0100

    Car class corrections

--- src/Car.java
+++ src/Car.java
@@ -1,11 +1,11 @@
 public class Car {
 
-    public static final String brand = "Seat";
+    public static final String BRAND = "Seat";
     public static String model;
-    public final int potency;
+    public final int POTENCY;
 
     public Car(int potencia) { // You only can initialize the potency attribute because it´s the only one who is not static.
-        this.potency = potencia;
+        this.POTENCY = potencia;
     }
 
     public static String brake() {
